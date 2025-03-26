3da348e253588e35c12729474131e24c2d0c4769
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-03-26 22:25:54 +0700

    create notification entity

---
+++ ecommerce/src/main/java/com/tpt/capstone_ecommerce/ecommerce/entity/Notification.java
@@ -0,0 +1,53 @@
+package com.tpt.capstone_ecommerce.ecommerce.entity;
+
+import com.tpt.capstone_ecommerce.ecommerce.enums.NOTIFICATION_TYPE;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "notifications")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "VARCHAR(36)", updatable = false, nullable = false)
+    private String id;
+
+    @Column(nullable = false, name = "message")
+    @NotBlank(message = "Message notification cannot be blank")
+    @Size(min = 1, max = 255, message = "Message notification length is invalid")
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    @NotNull(message = "Notification type cannot be null")
+    private NOTIFICATION_TYPE type;
+
+    @Column(length = 36, name = "reference_id", nullable = false)
+    private String referenceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User userId;
+
+    @Column(nullable = false, name = "created_at", updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true, name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ ecommerce/src/main/java/com/tpt/capstone_ecommerce/ecommerce/enums/NOTIFICATION_TYPE.java
@@ -0,0 +1,8 @@
+package com.tpt.capstone_ecommerce.ecommerce.enums;
+
+public enum NOTIFICATION_TYPE {
+    ORDER_SUCCESS,
+    ORDER_CANCELED,
+    NEW_REVIEW,
+    REPLY_REVIEW,
+}
