722a60bfb2551a01cf087acdb40ac4232e5693f3
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2025-05-08 11:21:00 +0700

    [feat/slow-registration] add thread for send mail

--- ecommerce/src/main/java/com/tpt/capstone_ecommerce/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/tpt/capstone_ecommerce/ecommerce/EcommerceApplication.java
@@ -2,7 +2,9 @@ package com.tpt.capstone_ecommerce.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class EcommerceApplication {
 
